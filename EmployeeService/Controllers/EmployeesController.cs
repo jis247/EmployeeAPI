07abd3ea72a744b8953e49b2411bbacd3238f476
Jis Thottan <jis7564@gmail.com>
2019-05-16 17:27:28 +0530

    commit 16052019

---
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace EmployeeService.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        public IEnumerable<Employee> Get()
+        {
+            using (PlayDBEntities entities = new PlayDBEntities())
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# EmployeeAPI
-using c# basic
